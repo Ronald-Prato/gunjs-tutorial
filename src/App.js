cc8cf01588a5e046945249d6658dd59a23827375
Ronald Prato <ronald.prato>
2022-03-16 10:20:58 -0500

    first commit with whole tutorial content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { gun } from './gun'
+import { v4 as uuidv4 } from 'uuid'
 
-function App() {
+const App = () => {
+  const [localMessage, setLocalMessage] = useState('')
+  const [messageList, setMessageList] = useState({})
+  const [showList, setShowList] = useState(false)
+
+  useEffect(() => {
+    gun
+      .get('Workbench')
+      .get('messages')
+      .map()
+      .on((message) => {
+        setShowList(false)
+        const newMessages = messageList
+        newMessages[message.id] = message
+
+        setMessageList(newMessages)
+        setShowList(true)
+      })
+  }, [])
+
+  const onChangeMessage = (event) => {
+    const value = event.target.value
+    setLocalMessage(value)
+  }
+
+  const onSend = (event) => {
+    event && event.preventDefault()
+
+    const uid = uuidv4()
+
+    const newMessage = {
+      id: uid,
+      message: localMessage,
+    }
+
+    gun.get('Workbench').get('messages').set(newMessage)
+    setLocalMessage('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-wrapper">
+      <h1>Send a message</h1>
+      <form onSubmit={onSend}>
+        <input value={localMessage} onChange={onChangeMessage} />
+        <button onClick={onSend}>Enviar</button>
+      </form>
+
+      <h2> Messages history </h2>
+      {showList &&
+        Object.values(messageList).map((m) => {
+          return <p key={m.id}>{m.message}</p>
+        })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/gun.js
@@ -0,0 +1,5 @@
+import Gun from 'gun'
+
+const gun = Gun('http://localhost:8000/gun')
+
+export { gun }
